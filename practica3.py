1c5d7a958e7e99c6d86578407227895828134045
ved-Johan <61301504+ved-Johan@users.noreply.github.com>
2020-03-11 21:50:22 -0600

    Add files via upload

---
+++ practica3.py
@@ -0,0 +1,50 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib
+import mysql.connector as mysql
+
+while(1):
+    e=""
+    i=0
+    conexion=mysql.connect(host='localhost',user='root',passwd='',db='datos')
+    operacion=conexion.cursor()
+    operacion.execute("SELECT * FROM webp;")
+    for pagina,estatus in operacion.fetchall():
+        print(pagina+" "+str(estatus))
+        if(estatus==0):
+            try:
+                url=urlopen(pagina)
+            except urllib.error.HTTPError:
+                continue
+            except UnicodeEncodeError:
+                continue
+            bs=BeautifulSoup(url.read(),'html.parser',from_encoding="iso-8859-1")
+            for enlaces in bs.find_all("a"):
+                s=enlaces.get("href")
+                try:
+                    if(s[0:4]=="http"):
+                        try:
+                            operacion.execute("insert into webp values('"+s+"',0);")
+                            print(s)
+                        except mysql.errors.IntegrityError:
+                            pass
+                        except mysql.errors.DataError:
+                            pass
+                    else:
+                        try:
+                            operacion.execute("insert into webp values('"+pagina+s+"',0);")
+                            print(pagina+s)
+                        except mysql.errors.IntegrityError:
+                            pass
+                        except mysql.errors.DataError:
+                            pass
+                except TypeError:
+                    pass
+            i=1
+            operacion.execute("update webp set estatus=1 where pagina='"+pagina+"';")
+            
+            conexion.commit()
+        
+    conexion.close()
+    if(i==0):
+        sys.exit()
